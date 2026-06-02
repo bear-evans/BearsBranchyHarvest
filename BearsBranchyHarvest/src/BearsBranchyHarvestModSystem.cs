527a457aa33e463af09fe76f4b33d64fc9ff3069
Bear Evans <bear-evans@outlook.com>
2026-06-02 12:10:12 -0400

    Moved Logic to AssetsFinalize
    Abandoned JSON editing due to repeated overwriting by mods.
    Now uses AssetsFinalize to iterate through leaf blocks registered and alters their drops in the C# objects.
    Inherent compatibility with anything that inherits from leaves.json or branchy.json.
    
    Tested to work with Wildcraft and Biodiversity

--- BearsBranchyHarvest/src/BearsBranchyHarvestModSystem.cs
+++ BearsBranchyHarvest/src/BearsBranchyHarvestModSystem.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using Newtonsoft.Json.Linq;
 using Vintagestory.API.Client;
@@ -142,11 +141,14 @@ namespace BearsBranchyHarvest
             Mod.Logger.Notification(Lang.Get("bearsbranchyharvest:hello"));
         }
 
+        /// <summary>
+        /// Hijacks the leaf block drops once they have been converted to C# objects but before they have been fully finalized. We do this here to avoid the JSON fiasco of mods patching and clobbering each other's JSON.
+        /// </summary>
         public override void AssetsFinalize(ICoreAPI api)
         {
             base.AssetsFinalize(api);
 
-            // no need to run this on anything but the server, clients will download the JSON
+            // no need to run this on anything but the server, clients will download the blocks
             if (api.Side != EnumAppSide.Server) {
                 return;
             }
@@ -159,7 +161,6 @@ namespace BearsBranchyHarvest
 
             // initialize variables out here so we can reuse the same memory addresses
             BranchyHarvestSettings settings = BranchyHarvestSettingsLoader.CurrentSettings;
-
             bool isBranchy;
 
             foreach (Block block in api.World.Blocks) {
@@ -191,91 +192,13 @@ namespace BearsBranchyHarvest
                         }
 
                         if ((settings.AllowLeafyDropWithKnife && !isBranchy) || (settings.AllowBranchyDropWithKnife && isBranchy)) {
-                            BlockDropItemStack leafDrop = GetLeafBlockDropStack(block);
+                            (bool, BlockDropItemStack) leafDrop = GetLeafBlockDropStack(block, api);
 
-                            block.Drops = block.Drops.Prepend(leafDrop) as BlockDropItemStack[];
+                            if (leafDrop.Item1) {
+                                block.Drops = block.Drops.Prepend(leafDrop.Item2).ToArray();
+                            }
                         }
                     }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Triggers once assets have been loaded and patched from all sources but before the assets are constructed into C# block objects. Our patches happen here.
-        ///
-        /// This gradually turned into a monolithic function. Woops.
-        /// </summary>
-        public override void AssetsLoaded(ICoreAPI api)
-        {
-            // no need to run this on anything but the server, clients will download the JSON
-            if (api.Side != EnumAppSide.Server) {
-                return;
-            }
-
-            if (BranchyHarvestSettingsLoader.CurrentSettings == null) {
-                Mod.Logger.Error(Lang.Get("bearsbranchyharvest:settings-missing-error"));
-                // couldn't find settings, abort
-                return;
-            }
-
-            // initialize variables out here so we can reuse the same memory addresses
-            BranchyHarvestSettings settings = BranchyHarvestSettingsLoader.CurrentSettings;
-
-            IAsset asset;
-            JToken token;
-            JArray? dropArray;
-            bool isPatched;
-
-            // cycle through the list of asset addresses we want to patch
-            foreach (AssetLocation assetLocation in settings.patchableAssetList) {
-                asset = api.Assets.TryGet(assetLocation);
-
-                if (asset == null) {
-                    Mod.Logger.Error($"{Lang.Get("bearsbranchyharvest:patch-error-asset-missing")}{assetLocation}");
-                    // The asset wasn't found. Skip it.
-                    continue;
-                }
-
-                isPatched = false;
-
-                // try to turn the JSON into a navigable object so we can traverse it, catching any errors so we can skip the patching gracefully
-                try {
-                    token = JToken.Parse(asset.ToText());
-
-                    if (token is JObject leafObject) {
-                        // the block code will help us sort out what logic to use. Unfortunately there's no one-size fits all patch for every leaf block.
-                        string? blockCode = leafObject["code"]?.ToString();
-
-                        if (blockCode == null) {
-                            continue; // abort if we can't even figure out what block it is.
-                        }
-                        // Handle Wildcraft code. Wildcraft has things a bit different from vanilla.
-                        //else if (assetLocation.Path.Contains("wildcrafttree")) {
-                        //}
-
-                        //// Guess what? Biodiversity has the same exact trees but in a completely different JSON configuration.
-                        //else if (assetLocation.Path.Contains("bdtree")) {
-                        //    PatchBiodiversityLeafBlock(leafObject, settings, assetLocation, asset, ref isPatched);
-                        //}
-
-                        // We have finally reached vanilla leaf block code.
-                        else {
-                            PatchLeafBlock(leafObject, settings, assetLocation, asset, ref isPatched);
-                        }
-
-                        // All done tweaking the json
-                        if (isPatched) {
-                            SubmitPatchedJSONObject(leafObject, assetLocation, asset);
-                        }
-                        else {
-                            // handle the case that a provided asset can't accept any of the configured modifications, such as if we are given a dirt block or something.
-                            Mod.Logger.Notification($"{Lang.Get("bearsbranchyharvest:patch-notification-no-patch")}{assetLocation}");
-                        }
-                    }
-                }
-                catch (Exception e) {
-                    // in case we run into a filesystem error or other horrible issue
-                    Mod.Logger.Error($"{Lang.Get("bearsbranchyharvest:patch-error-asset-malformed")} {Lang.Get("bearsbranchyharvest:patch-bit-asset")} {assetLocation} {Lang.Get("bearsbranchyharvest:patch-bit-exception")} {e.Message}");
                 }
             }
         }
@@ -286,11 +209,20 @@ namespace BearsBranchyHarvest
         private string GetFriendlyLeafDropName(string blockCode)
         {
             string[] subStrings = blockCode.Split("-");
-            subStrings[^1] = "placed";
+            // HACK: this is pretty fragile. Need to do some more sophisticated targeting logic to get the state
+            if (subStrings[1].Contains("bambooleaves")) {
+                subStrings[^1] = "placed";
+            }
+            else {
+                subStrings[^2] = "placed";
+            }
             return string.Join("-", subStrings);
         }
 
-        private BlockDropItemStack GetLeafBlockDropStack(Block block)
+        /// <summary>
+        /// Helper function fabricate a BlockDropItemStack designed for leaf block drops.
+        /// </summary>
+        private (bool, BlockDropItemStack) GetLeafBlockDropStack(Block block, ICoreAPI api)
         {
             BlockDropItemStack leafDrop = new() {
                 Type = EnumItemClass.Block,
@@ -300,105 +232,9 @@ namespace BearsBranchyHarvest
                 LastDrop = true
             };
 
-            return leafDrop;
-        }
+            bool didSucceed = leafDrop.Resolve(api.World, "", AssetLocation.Create(leafDrop.Code));
 
-        /// <summary>
-        /// Patches the C# incarnation of a leaf block. This ensures no other mods can clobber the patches.
-        /// </summary>
-        private void PatchLeafCSharpObject(Block leafBlock, BranchyHarvestSettings settings)
-        {
-        }
-
-        /// <summary>
-        /// Handles the main patching logic of a vanilla leaf block JSON object.
-        /// </summary>
-        private void PatchLeafBlock(JObject leafObject, BranchyHarvestSettings settings, AssetLocation assetLoc, IAsset asset, ref bool isPatched)
-        {
-            string? blockCode = leafObject["code"]?.ToString();
-
-            if (blockCode == null) {
-                return;
-            }
-
-            if (leafObject["drops"] is not JArray dropArray) { return; } // fascinating alteration made by CodeMaid. Didn't know you could condense it like this
-
-            // add knife drops if enabled. These have to go first in the drop array and be labeled with lastDrop
-            // so they execute first and stop stick and seed drops
-            if (settings.AllowBranchyDropWithKnife) {
-                if (blockCode.Equals("leavesbranchy")) {
-                    AddBlockDrop(dropArray, "leavesbranchy-placed-{wood}");
-                    isPatched = true;
-                }
-            }
-
-            // leafy blocks have all sorts of naming schemes and I couldn't figure out a regular expression that could handle them all
-            if (settings.AllowLeafyDropWithKnife) {
-                if (blockCode.Equals("leaves")) {
-                    AddBlockDrop(dropArray, "leaves-placed-{wood}");
-                    isPatched = true;
-                }
-                else if (blockCode.Equals("bambooleaves")) {
-                    AddBlockDrop(dropArray, "bambooleaves-{color}-placed");
-                    isPatched = true;
-                }
-                else if (blockCode.Equals("leavesnarrow")) {
-                    AddBlockDrop(dropArray, "leavesnarrow-placed-{wood}");
-                    isPatched = true;
-                }
-            }
-
-            // iterate through the JSON drop array and search for seeds and sticks so we can modify them
-            if (settings.AlterStickDrops) {
-                foreach (JObject dropPreset in dropArray) {
-                    string? dropCode = dropPreset["code"]?.ToString();
-                    if (dropCode is not null and "stick") {
-                        if (dropPreset["quantity"] is JObject dropObject) {
-                            if (blockCode.Contains("branchy")) {
-                                PatchStickAmounts(dropObject, settings, LeafBlockType.Branchy);
-                            }
-                            else {
-                                PatchStickAmounts(dropObject, settings, LeafBlockType.Leafy);
-                            }
-                            isPatched = true;
-                        }
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Inserts a knife drop object at the beginning of the drop array, only needing a block code.
-        /// </summary>
-        private void AddBlockDrop(JArray dropArray, string blockToDrop)
-        {
-            DropPresetObject branchyDrop = new("block", blockToDrop, "knife", 1f, 0f, true);
-            dropArray.AddFirst(JToken.FromObject(branchyDrop));
-        }
-
-        /// <summary>
-        /// Patches the average and variance properties of the provided stick drop JSON object.
-        /// </summary>
-        private void PatchStickAmounts(JObject dropObject, BranchyHarvestSettings settings, LeafBlockType blockType)
-        {
-            dropObject["avg"] = blockType == LeafBlockType.Branchy ? settings.BranchyStickAverage : settings.LeafyStickAverage;
-
-            // not every quantity object has "var", so we need to do a little extra logic
-            if (dropObject.ContainsKey("var")) {
-                dropObject["var"] = blockType == LeafBlockType.Branchy ? settings.BranchyStickVariance : settings.LeafyStickVariance;
-            }
-            else {
-                dropObject.Add("var", JToken.FromObject(blockType == LeafBlockType.Branchy ? settings.BranchyStickVariance : settings.LeafyStickVariance));
-            }
-        }
-
-        /// <summary>
-        /// Submits the altered JSON object back into the asset data.
-        /// </summary>
-        private void SubmitPatchedJSONObject(JObject json, AssetLocation assetLoc, IAsset asset)
-        {
-            Mod.Logger.Notification($"{Lang.Get("bearsbranchyharvest:patch-notification-patch-success")}{assetLoc}");
-            asset.Data = System.Text.Encoding.UTF8.GetBytes(json.ToString());
+            return (didSucceed, leafDrop);
         }
     }
 
